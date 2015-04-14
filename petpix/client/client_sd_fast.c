c91f00c87a5268e4e3fc233f09ae9997d799722a
Michael Hill <gubbish@Michaels-MacBook-Pro.local>
2015-04-14 08:18:42 -0700

    added fast client code, petpix firmware

---
+++ petpix/client/client_sd_fast.c
@@ -0,0 +1,277 @@
+/**********
+
+petpix_sdclient
+-----
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; version 2 of the License.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+    
+    More information about GNU can be found at the following website:
+    http://www.gnu.org/
+    
+**********/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <peekpoke.h> 
+#include <time.h>
+#include <pet.h>
+#include <string.h>
+//#include "spockw.h"
+
+
+
+
+/*
+original basic program
+
+5 poke 59468,peek(59468) or 1
+6 poke 59467, peek(59467) and 227
+8 poke 59468,peek(59468) or 224
+10 poke 59467,peek(59467) and 254
+20 a = peek(59457)
+30 poke 32768,45
+40 if(peek(59469) and 2) > 0 then 60
+42 poke 32768,92
+44 if(peek(59469) and 2) > 0 then 60
+45 poke 32768,93
+46 if(peek(59469) and 2) > 0 then 60
+47 poke 32768,47
+48 if(peek(59469) and 2) > 0 then 60
+50 goto 30
+60 for x = 1 to 1000 step 2
+80 poke 59468,(peek(59468) and 31) or 192
+90 a = peek(59457)
+100 poke 32767+x,a
+110 poke 59468, peek(59468) or 224
+120 b = peek(59457)
+130 poke 32767+x+1,b
+140 next x
+150 goto 5
+*/
+
+//#define CA1_TRIGGER     59468
+//#define CA1_DATA_LATCH  59467
+//#define CA1_STATUS      59469
+//#define USERPORT_DATA   59457
+//#define VMEM_START      32768
+
+#define CA1_TRIGGER     0xE84C
+#define CA1_DATA_LATCH  0xE84B
+#define CA1_STATUS      0xE84D
+#define USERPORT_DATA   0xE841
+#define VMEM_START      0x8000
+#define VMEM_START_2    0x8100
+#define VMEM_START_3    0x8200
+#define VMEM_START_4    0x82E8
+
+
+#define RUN_STOP 3
+
+int main (void)
+{
+    unsigned char data;
+    int x;
+    
+    while(1)
+    {
+    
+    data = PEEK(CA1_TRIGGER);
+    data = data | 0x01;
+    POKE(CA1_TRIGGER, data);
+    
+    // disable shift register modes
+    data = PEEK(CA1_DATA_LATCH);
+    data = data & 227;
+    POKE(CA1_DATA_LATCH, data);
+    
+    // set CB2 line low poke 59468,peek(59468) or 224
+    data = PEEK(CA1_TRIGGER);
+    data = data | 224;
+    POKE(CA1_TRIGGER, data);
+    
+    // data not latched
+    data = PEEK(CA1_DATA_LATCH);
+    data = data & 254;
+    POKE(CA1_DATA_LATCH, data);
+    
+    // read data, reset CA1 flag bit
+    data = PEEK(USERPORT_DATA);
+    
+        // wait for a byte to be ready
+        data = PEEK(CA1_STATUS);
+        data = data & 2;
+        while (data == 0)
+        {
+            data = PEEK(CA1_STATUS);
+            data = data & 2;
+        }
+        
+        /*
+         60 for x = 1 to 1000 step 2
+         80 poke 59468,(peek(59468) and 31) or 192
+         90 a = peek(59457)
+         100 poke 32767+x,a
+         110 poke 59468, peek(59468) or 224
+         120 b = peek(59457)
+         130 poke 32767+x+1,b
+         140 next x
+        */
+        
+        /*
+        for (x = 0; x < 1000; x += 2)
+        {
+            // set cb2 line low poke 59468,(peek(59468) and 31) or 192
+            data = PEEK(CA1_TRIGGER);
+            data = (data & 31) | 192;
+            POKE(CA1_TRIGGER, data);
+            
+            data = PEEK(USERPORT_DATA);
+            POKE(VMEM_START+x, data);
+            
+            // set cb2 line high
+            data = PEEK(CA1_TRIGGER);
+            data = data | 224;
+            POKE(CA1_TRIGGER, data);
+            
+            data = PEEK(USERPORT_DATA);
+            POKE(VMEM_START+x+1, data);
+        }
+        */
+        
+    // load 1000 bytes from the userport
+        asm("ldx #$00");
+        
+        // prepare trigger value
+        asm("lda %w", CA1_TRIGGER);
+        asm("ora #$E0");
+        asm("tay");
+        
+    jumper:
+        
+        // set CB2 line low
+        asm("tya");
+        asm("and #$DF");
+        asm("sta %w", CA1_TRIGGER);
+        
+        // load byte from userport, save to video memory
+        asm("lda %w", USERPORT_DATA);
+        asm("sta %w,x", VMEM_START);
+        
+        // step video mem pointer
+        asm("inx");
+        
+        // set CB2 line high
+        asm("tya");
+        asm("ora #$E0");
+        asm("sta %w", CA1_TRIGGER);
+        
+        // load byte from userport, save to video memory
+        asm("lda %w", USERPORT_DATA);
+        asm("sta %w,x", VMEM_START);
+        
+        // step video mem pointer
+        asm("inx");
+        // jump if x != 0
+        asm("bne %g", jumper);
+    jumper2:
+        // set CB2 line low
+        asm("tya");
+        asm("and #$DF");
+        asm("sta %w", CA1_TRIGGER);
+        
+        // load byte from userport, save to video memory
+        asm("lda %w", USERPORT_DATA);
+        asm("sta %w,x", VMEM_START_2);
+        
+        // step video mem pointer
+        asm("inx");
+        
+        // set CB2 line high
+        asm("tya");
+        asm("ora #$E0");
+        asm("sta %w", CA1_TRIGGER);
+        
+        // load byte from userport, save to video memory
+        asm("lda %w", USERPORT_DATA);
+        asm("sta %w,x", VMEM_START_2);
+        
+        // step video mem pointer
+        asm("inx");
+        // jump if x != 0
+        asm("bne %g", jumper2);
+    jumper3:
+        // set CB2 line low
+        asm("tya");
+        asm("and #$DF");
+        asm("sta %w", CA1_TRIGGER);
+        
+        // load byte from userport, save to video memory
+        asm("lda %w", USERPORT_DATA);
+        asm("sta %w,x", VMEM_START_3);
+        
+        // step video mem pointer
+        asm("inx");
+        
+        // set CB2 line high
+        asm("tya");
+        asm("ora #$E0");
+        asm("sta %w", CA1_TRIGGER);
+        
+        // load byte from userport, save to video memory
+        asm("lda %w", USERPORT_DATA);
+        asm("sta %w,x", VMEM_START_3);
+        
+        // step video mem pointer
+        asm("inx");
+        // jump if x != 0
+        asm("bne %g", jumper3);
+        
+        // set offset for last section
+        asm("ldx #$18");
+    jumper4:
+        
+        // set CB2 line low
+        asm("tya");
+        asm("and #$DF");
+        asm("sta %w", CA1_TRIGGER);
+        
+        // load byte from userport, save to video memory
+        asm("lda %w", USERPORT_DATA);
+        asm("sta %w,x", VMEM_START_4);
+        
+        // step video mem pointer
+        asm("inx");
+        
+        // set CB2 line high
+        asm("tya");
+        asm("ora #$E0");
+        asm("sta %w", CA1_TRIGGER);
+        
+        // load byte from userport, save to video memory
+        asm("lda %w", USERPORT_DATA);
+        asm("sta %w,x", VMEM_START_4);
+        
+        // step video mem pointer
+        asm("inx");
+        // jump if x != 0
+        asm("bne %g", jumper4);
+        
+
+    }
+	return EXIT_SUCCESS;
+}
+
+
---
+++ petpix/firmware/PETpix2.c
@@ -0,0 +1,186 @@
+/*
+ * PETpix2.c
+ *
+ * Created: 2/2/2013 9:14:06 PM
+ *  Author: Michael Hill
+ */ 
+
+//#include "lz.h"
+#include "UART_routines.h"
+#include <avr/io.h>
+#include <util/delay.h>
+
+//#define ACTIVE_LOW 1
+//#define DEBUG 1
+
+//#define USE_COMPRESSED_BUFFER 1
+
+int main(void)
+{
+    unsigned char txReady;
+    unsigned char txNotReady;
+    unsigned char transmitColorBytes = 0;
+    
+#ifdef ACTIVE_LOW
+    txReady = 0x00;
+    txNotReady = 0x01;
+#else
+    txReady = 0x01;
+    txNotReady = 0x00;
+#endif
+    
+    // set CA1 output
+    DDRA = 0x01;
+    // set low
+    PORTA = txReady;
+    
+    uart0_init(2); // 333kbaud
+    //uart0_init(51); // 19.2kbaud
+    
+    // delay to get falling edge
+    _delay_loop_2(1000);
+    
+    unsigned char byte;
+    unsigned int numBytes;
+    
+#ifdef USE_COMPRESSED_BUFFER
+    unsigned char compressedBuffer[1100];
+#endif
+    
+    unsigned char buffer[2000];
+    unsigned char page = 0;
+	unsigned char color = 0;
+    int i;
+    while(1)
+    {
+        // set CA1 line low
+        PORTA = txNotReady;
+        byte = 0x00;
+        
+#ifdef DEBUG
+        transmitHex(CHAR, txReady);
+        transmitHex(CHAR, txNotReady);
+        transmitString("\r\n");
+#endif
+        
+        
+        transmitByte(':');
+    
+        // skip over buffer bytes
+        while (byte != 0xff)
+        {
+            byte = receiveByte();
+        }
+        
+        while (byte != 0x00)
+        {
+            byte = receiveByte();
+        }
+        
+#ifdef USE_COMPRESSED_BUFFER
+        buffer[0] = receiveByte();
+        buffer[1] = receiveByte();
+        
+        numBytes = buffer[0];
+        numBytes = numBytes << 8;
+        numBytes += buffer[1];
+        
+        //transmitHex(INT, numBytes);
+
+        for (i = 0; i < numBytes; i++)
+        {
+            compressedBuffer[i] = receiveByte();
+        }
+        
+        transmitHex(INT, numBytes);
+        //Huffman_Uncompress(compressedBuffer, buffer, numBytes, 1000);
+        //RLE_Uncompress(compressedBuffer, buffer, numBytes);
+        LZ_Uncompress(compressedBuffer, buffer, numBytes);
+#else
+
+        if (transmitColorBytes == 1)
+        {
+            numBytes = 2000;
+            for (i = 0; i < numBytes; i += 2)
+            {
+                buffer[i] = receiveByte();
+                buffer[i+1] = receiveByte();
+            }
+        }
+        else
+        {
+            numBytes = 1000;
+            for (i = 0; i < numBytes; i++)
+            {
+                buffer[i] = receiveByte();
+                byte = receiveByte();
+            }
+        }
+        
+        // TEST TEST
+        //_delay_loop_2(2000);
+#endif
+        
+        int j = numBytes;
+        unsigned char cv, currentVal;
+        
+        // done getting data
+        DDRC = 0xFF;
+        PORTA = txReady;
+        
+#ifdef DEBUG
+        transmitString("waiting for CB2");
+#endif
+    
+        
+        // now wait for CB2 to go low
+        cv = PINA;
+        cv = cv & 0x02;
+        while (cv != 0x00)
+        {
+            cv = PINA;
+            cv = cv & 0x02;
+        }
+        
+        //transmitString("got CB2 low");
+        
+        // indicate not ready, test
+        PORTA = txNotReady;
+        
+        
+        // CB2 is now low
+        currentVal = cv;
+        for (i = 0; i < j; i++)
+        {
+            // output the current byte
+            PORTC = buffer[i];
+            
+            // wait for output to toggle
+            cv = PINA;
+            cv = cv & 0x02;
+            while (cv == 0x00)
+            {
+                cv = PINA;
+                cv = cv & 0x02;
+            }
+            
+            i++;
+            PORTC = buffer[i];
+            
+            //transmitHex(INT, i);
+            
+            if (i < j-1)
+            {
+                cv = PINA;
+                cv = cv & 0x02;
+                while (cv != 0x00)
+                {
+                    cv = PINA;
+                    cv = cv & 0x02;
+                }
+            }
+            
+            // the value has toggled, send next byte
+        }
+    }
+}
