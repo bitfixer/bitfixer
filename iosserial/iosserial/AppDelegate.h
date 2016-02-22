f9c21595a4d4af041fe07e22392064986a4fdbab
Michael Hill <gubbish@Michaels-MacBook-Pro.local>
2016-02-21 18:13:55 -0800

    added stuff

---
+++ iosserial/iosserial/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iosserial
+//
+//  Created by Michael Hill on 2/14/16.
+//  Copyright (c) 2016 Michael Hill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iosserial/iosserial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iosserial
+//
+//  Created by Michael Hill on 2/14/16.
+//  Copyright (c) 2016 Michael Hill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
