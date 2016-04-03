202428a39435c5cce66cab381a071b148a588b67
Michael Hill <gubbish@Michaels-MacBook-Pro.local>
2016-04-03 12:07:12 -0700

    cleanup of unused code

--- userportserial/client/client.c
+++ userportserial/client/client.c
@@ -1,6 +1,7 @@
 /**********
 
-petpix_sdclient
+client.c
+userport serial clients
 -----
 
     This program is free software; you can redistribute it and/or modify
@@ -30,48 +31,7 @@ petpix_sdclient
 #include <c64.h>
 #include <string.h>
 
-
-/*
-original basic program
-
-5 poke 59468,peek(59468) or 1
-6 poke 59467, peek(59467) and 227
-8 poke 59468,peek(59468) or 224
-10 poke 59467,peek(59467) and 254
-20 a = peek(59457)
-30 poke 32768,45
-40 if(peek(59469) and 2) > 0 then 60
-42 poke 32768,92
-44 if(peek(59469) and 2) > 0 then 60
-45 poke 32768,93
-46 if(peek(59469) and 2) > 0 then 60
-47 poke 32768,47
-48 if(peek(59469) and 2) > 0 then 60
-50 goto 30
-60 for x = 1 to 1000 step 2
-80 poke 59468,(peek(59468) and 31) or 192
-90 a = peek(59457)
-100 poke 32767+x,a
-110 poke 59468, peek(59468) or 224
-120 b = peek(59457)
-130 poke 32767+x+1,b
-140 next x
-150 goto 5
-*/
-
-//#define CA1_TRIGGER     59468
-//#define CA1_DATA_LATCH  59467
-//#define CA1_STATUS      59469
-//#define USERPORT_DATA   59457
-//#define VMEM_START      32768
-
-//#define CA1_TRIGGER     0xE84C
-//#define CA1_DATA_LATCH  0xE84B
-//#define CA1_STATUS      0xE84D
-//#define USERPORT_DATA   0xE841
-//#define VMEM_START      0x8000
-
-
+// memory addresses for I/O
 #define PORTB           0xDD01
 #define DDRB            0xDD03
 #define PORTA           0xDD00
@@ -98,13 +58,6 @@ int main (void)
         colormem[x] = 0;
     }
     
-    /*
-    // disable flag interrupt
-    data = *icr;
-    data = data & 0xEF;
-    *icr = data;
-    */
-    
     // set portb (userport data) to input
     POKE(DDRB, 0x00);
     
@@ -114,40 +67,11 @@ int main (void)
     // set PA2 high
     POKE(PORTA, 0x04);
     
-    /*
-    data = PEEK(CA1_TRIGGER);
-    data = data | 0x01;
-    POKE(CA1_TRIGGER, data);
-    
-    // disable shift register modes
-    data = PEEK(CA1_DATA_LATCH);
-    data = data & 227;
-    POKE(CA1_DATA_LATCH, data);
-    
-    // set CB2 line high poke 59468,peek(59468) or 224
-    data = PEEK(CA1_TRIGGER);
-    data = data | 224;
-    POKE(CA1_TRIGGER, data);
-    
-    // data not latched
-    data = PEEK(CA1_DATA_LATCH);
-    data = data & 254;
-    POKE(CA1_DATA_LATCH, data);
-    
-    // read data, reset CA1 flag bit
-    data = PEEK(USERPORT_DATA);
-    */
-    
     // read ICR to clear flag
     data = PEEK(ICR);
     
     while (!done)
     {
-        //data = *icr & 0x10;
-        //data = PEEK(PORTB);
-        //*cursorpos = data;
-        
-        
         // look for FLAG2 (data ready)
         data = PEEK(ICR);
         data = data & 0x10;
@@ -171,69 +95,8 @@ int main (void)
         {
             done = 1;
         }
-        
-        
-        /*
-        // delay
-        for (x = 0; x < 100; x++)
-        {
-            *cursorpos = spinner++;
-        }
-        */
          
         POKE(PORTA, 0x04);
-        
-        
-        /*
-        data = data & 0x10;
-        
-        while (data == 0)
-        {
-            *cursorpos = spinner++;
-            data = PEEK(ICR);
-            data = data & 0x10;
-        }
-        */
-        
-        
-        /*
-        // wait for a byte to be ready
-        data = PEEK(CA1_STATUS);
-        data = data & 2;
-        
-        while (data != 0)
-        {
-            *cursorpos = spinner++;
-            data = PEEK(CA1_STATUS);
-            data = data & 2;
-            // *cursorpos = '0'+data;
-        }
-        
-        // read the byte
-        data = PEEK(USERPORT_DATA);
-        *cursorpos = data;
-        cursorpos++;
-        
-        // set CB2 line low to indicate byte has been read
-        // poke 59468,(peek(59468) and 31) or 192
-        data = PEEK(CA1_TRIGGER);
-        data = (data & 31) | 192;
-        POKE(CA1_TRIGGER, data);
-        
-        // wait for CA1 line low
-        data = PEEK(CA1_STATUS);
-        data = data & 2;
-        while (data == 0)
-        {
-            data = PEEK(CA1_STATUS);
-            data = data & 2;
-        }
-        
-        // set CB2 line high
-        data = PEEK(CA1_TRIGGER);
-        data = data | 224;
-        POKE(CA1_TRIGGER, data);
-        */
     }
     
 	return EXIT_SUCCESS;
--- userportserial/upserial.c
+++ userportserial/upserial.c
@@ -11,9 +11,6 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-//#define ACTIVE_LOW 1
-//#define DEBUG 1
-
 #define TX_READY        0x00
 #define TX_NOT_READY    0x01
 
@@ -74,7 +71,7 @@ bool remote_ready()
     return false;
 }
 
-int main(void)
+void init()
 {
     // set CA1 output
     DDRA = 0x01;
@@ -84,11 +81,16 @@ int main(void)
     // all data lines output
     DDRC = 0xFF;
     uart0_init(51); // 19.2kbaud
+}
+
+int main(void)
+{
+    init();
     signal_byte_not_ready();
     
     // delay to get falling edge
     _delay_loop_2(1000);
-    PORTC = 0x00;
+    output_byte(0x00);
     
     circularBuffer byteBuffer;
     circularBuffer_init(&byteBuffer);
@@ -136,8 +138,6 @@ int main(void)
                 
                 // change state
                 st = BYTE_READY;
-                
-                //call1(outputBuffer, push, 'a');
             }
         }
         else if (st == BYTE_READY)
@@ -149,8 +149,6 @@ int main(void)
                 signal_byte_not_ready();
                 
                 st = WAIT_FOR_REMOTE_READY;
-                
-                //call1(outputBuffer, push, 'b');
             }
         }
         else if (st == WAIT_FOR_REMOTE_READY)
@@ -160,153 +158,7 @@ int main(void)
             {
                 // return to idle state
                 st = IDLE;
-                
-                //call1(outputBuffer, push, 'c');
             }
         }
     }
-    
-    
-    /*
-    const char *str = "What's up dude? Fart";
-    
-    // test
-    for (unsigned char i = 0; i < 10; i++)
-    {
-        (byteBuffer.push)(&byteBuffer, str[i]);
-    }
-    
-    for (int i = 0; i < 10; i++)
-    {
-        int s;
-        unsigned char c;
-        
-        s = (byteBuffer.getSize)(&byteBuffer);
-        transmitHex(INT, s);
-        if (s > 0)
-        {
-            c = (byteBuffer.pop)(&byteBuffer);
-            transmitString((unsigned char *)"popped: ");
-            transmitHex(CHAR, c);
-            transmitString((unsigned char *)" ");
-            transmitByte(c);
-            transmitString((unsigned char *)"\r\n");
-        }
-    }
-    */
-    
-    
-    
-    
-    
-    /*
-    
-    while(1)
-    {
-        // set CA1 line low
-        PORTA = txNotReady;
-        byte = 0x00;
-        
-#ifdef DEBUG
-        transmitHex(CHAR, txReady);
-        transmitHex(CHAR, txNotReady);
-        transmitString("\r\n");
-#endif
-        
-        
-        transmitByte(':');
-    
-        // skip over buffer bytes
-        while (byte != 0xff)
-        {
-            byte = receiveByte();
-        }
-        
-        while (byte != 0x00)
-        {
-            byte = receiveByte();
-        }
-
-        if (transmitColorBytes == 1)
-        {
-            numBytes = 2000;
-            for (i = 0; i < numBytes; i += 2)
-            {
-                buffer[i] = receiveByte();
-                buffer[i+1] = receiveByte();
-            }
-        }
-        else
-        {
-            numBytes = 1000;
-            for (i = 0; i < numBytes; i++)
-            {
-                buffer[i] = receiveByte();
-                byte = receiveByte();
-            }
-        }
-        
-        int j = numBytes;
-        unsigned char cv, currentVal;
-        
-        // done getting data
-        DDRC = 0xFF;
-        PORTA = txReady;
-        
-#ifdef DEBUG
-        transmitString("waiting for CB2");
-#endif
-    
-        
-        // now wait for CB2 to go low
-        cv = PINA;
-        cv = cv & 0x02;
-        while (cv != 0x00)
-        {
-            cv = PINA;
-            cv = cv & 0x02;
-        }
-        
-        //transmitString("got CB2 low");
-        
-        // indicate not ready, test
-        PORTA = txNotReady;
-        
-        
-        // CB2 is now low
-        currentVal = cv;
-        for (i = 0; i < j; i++)
-        {
-            // output the current byte
-            PORTC = buffer[i];
-            
-            // wait for output to toggle
-            cv = PINA;
-            cv = cv & 0x02;
-            while (cv == 0x00)
-            {
-                cv = PINA;
-                cv = cv & 0x02;
-            }
-            
-            i++;
-            PORTC = buffer[i];
-            
-            //transmitHex(INT, i);
-            
-            if (i < j-1)
-            {
-                cv = PINA;
-                cv = cv & 0x02;
-                while (cv != 0x00)
-                {
-                    cv = PINA;
-                    cv = cv & 0x02;
-                }
-            }
-            
-            // the value has toggled, send next byte
-        }
-    }
-    */
 }
