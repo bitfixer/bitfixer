053d9eb9b1b62a810556d3f4745f89b139c53867
Michael Hill <gubbish@Michaels-MacBook-Pro.local>
2016-04-14 16:19:00 -0400

    faster client

--- userportserial/client/client.c
+++ userportserial/client/client.c
@@ -34,130 +34,67 @@ userport serial clients
 
 #define VMEM_START      0x0400
 #define SIDADDR         0xD400
+#define CIA2_ICR        0xDD0D
+#define CIA2_PRA        0xDD00
+#define CIA2_PRB        0xDD01
 #define RUN_STOP 3
-
-typedef struct
-{
-    unsigned char buffer[3];
-    unsigned char *type;
-    unsigned char *sid_offset;
-    unsigned char *value;
-    unsigned char bytes_recv;
-    unsigned char ready;
-} control_packet;
-
-void init_control_packet(control_packet *pkt)
-{
-    pkt->type = &pkt->buffer[0];
-    pkt->sid_offset = &pkt->buffer[1];
-    pkt->value = &pkt->buffer[2];
-    
-    *pkt->type = 0;
-    *pkt->sid_offset = 0;
-    *pkt->value = 0;
-    pkt->bytes_recv = 0;
-    pkt->ready = 0;
-}
-
-void add_byte_to_control_packet(unsigned char byte, control_packet *pkt)
-{
-    pkt->buffer[pkt->bytes_recv] = byte;
-    pkt->bytes_recv++;
-    
-    if (pkt->bytes_recv >= 3)
-    {
-        pkt->bytes_recv = 0;
-        pkt->ready = 1;
-    }
-}
-
-void process_control_packet(control_packet *pkt)
-{
-    // process packet according to type
-    if (*pkt->type == 1)
-    {
-        unsigned char *sid = (unsigned char *)SIDADDR;
-        sid[*pkt->sid_offset] = *pkt->value;
-    }
-}
 
 int main (void)
 {
-    unsigned char data;
-    unsigned char done = 0;
-    unsigned char spinner = 0;
-    unsigned char *cursorpos = (unsigned char *)VMEM_START;
     unsigned char *colormem = COLOR_RAM;
+    unsigned char *cursorpos = (unsigned char *)VMEM_START;
     int x;
-    
-    control_packet pkt;
-    init_control_packet(&pkt);
-    
-    data = test();
-    
     for (x = 0; x < 1000; x++)
     {
         colormem[x] = 0;
     }
     
+    // initialize serial port
     serial_init();
     
-    while (!done)
-    {
-        // check for a ready control packet
-        if (pkt.ready)
-        {
-            process_control_packet(&pkt);
-            pkt.ready = 0; // packet no longer ready
-        }
-        
-        if (serial_byte_ready())
-        {
-            // process byte
-            data = serial_read_byte();
-            
-            // do stuff
-            if ((data == 'q' || data == 'Q') && pkt.bytes_recv == 0)
-            {
-                done = 1;
-            }
-            else
-            {
-                add_byte_to_control_packet(data, &pkt);
-                *cursorpos = data;
-                cursorpos++;
-            }
-            
-            serial_done_reading();
-        }
-        else
-        {
-            // idle, do background tasks
-            *cursorpos = spinner++;
-        }
-    }
+    asm("ldx #$00");
+    asm("ldy #$04");
+loopstart:
+    // read one byte
+    asm("lda %w", CIA2_ICR);
+    asm("and #$10"); // and the flag bit in ICR
+    asm("beq %g", loopstart);
+    
+firstbyte:
+    
+    //cursorpos[0] = 'A';
+    // read byte from port B
+    asm("lda %w", CIA2_PRB);
+    
+    // poke value to video memory
+    asm("sta %w", VMEM_START);
+    
+    // transfer x to accumulator
+    asm("txa");
+    asm("sta %w", CIA2_PRA);
+    
+    // transfer y to accumulator
+    asm("tya");
+    
+    // some nops for delay
+    asm("nop");
+    asm("nop");
+    asm("nop");
+    asm("nop");
+    //asm("nop");
+    
+    asm("sta %w", CIA2_PRA);
+    
+    asm("jmp %g", loopstart);
     
     
-    /*
-    while (!done)
-    {
-        if (serial_byte_ready())
-        {
-            data = serial_read_byte();
-            *cursorpos = data;
-            cursorpos++;
-            if (data == 'q' || data == 'Q')
-            {
-                done = 1;
-            }
-            serial_done_reading();
-        }
-        else
-        {
-            *cursorpos = spinner++;
-        }
-    }
-    */
+    // read another byte
+    
+    
+    // poke value
+    
+    
+    // do it again
     
     
 	return EXIT_SUCCESS;
