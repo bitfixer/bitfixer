28701826f0efa358c51f4879134e29569ce99d13
Michael Hill <gubbish@Michaels-MacBook-Pro.local>
2016-04-24 10:22:50 -0700

    added binary data convert

---
+++ binaryDataConvert/binaryDataConvert/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  binaryDataConvert
+//
+//  Created by Michael Hill on 4/15/16.
+//  Copyright (c) 2016 Michael Hill. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
