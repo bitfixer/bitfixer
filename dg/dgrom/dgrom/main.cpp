246f09a3addbd8e0249cbb26ec433f43607b289e
Michael Hill <gubbish@Michaels-MacBook-Pro.local>
2017-02-06 23:00:48 -0800

    dgrom dump program

---
+++ dg/dgrom/dgrom/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  dgrom
+//
+//  Created by Michael Hill on 1/16/17.
+//  Copyright © 2017 Michael Hill. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
