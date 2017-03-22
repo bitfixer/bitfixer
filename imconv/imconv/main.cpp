f0b7a6b570d86ea99c250e33c97040f31d90f63a
Michael Hill <gubbish@Michaels-MacBook-Pro.local>
2017-03-21 22:44:57 -0700

    moved c64 image code to shared

--- imconv/imconv/main.cpp
+++ imconv/imconv/main.cpp
@@ -17,38 +17,7 @@
 #include <string.h>
 #include <unistd.h>
 #include "StringTools.h"
-
-unsigned char c64_colors[] =
-{
-    0,      0,      0,
-    255,    255,    255,
-    136,    0,      0,
-    170,    255,    238,
-    204,    68,     204,
-    0,      204,    85,
-    0,      0,      170,
-    238,    238,    119,
-    221,    136,    85,
-    102,    68,     0,
-    255,    119,    119,
-    51,     51,     51,
-    119,    119,    119,
-    170,    255,    102,
-    0,      136,    255,
-    187,    187,    187
-};
-
-int num_64_colors = 16;
-
-unsigned char cga_colors[] =
-{
-    0,      0,      0,
-    255,    255,    255,
-    0x55,   0xFF,   0xFF,
-    0xFF,   0x55,   0xFF
-};
-
-int num_cga_colors = 4;
+#include "c64_colors.h"
 
 int main(int argc, const char * argv[]) {
     
---
+++ shared/c64_colors.h
@@ -0,0 +1,34 @@
+//
+//  c64_colors.h
+//  imconv
+//
+//  Created by Michael Hill on 3/21/17.
+//  Copyright © 2017 Michael Hill. All rights reserved.
+//
+
+#ifndef c64_colors_h
+#define c64_colors_h
+
+unsigned char c64_colors[] =
+{
+    0,      0,      0,
+    255,    255,    255,
+    136,    0,      0,
+    170,    255,    238,
+    204,    68,     204,
+    0,      204,    85,
+    0,      0,      170,
+    238,    238,    119,
+    221,    136,    85,
+    102,    68,     0,
+    255,    119,    119,
+    51,     51,     51,
+    119,    119,    119,
+    170,    255,    102,
+    0,      136,    255,
+    187,    187,    187
+};
+
+int num_64_colors = 16;
+
+#endif /* c64_colors_h */
