66fd74e50e3339113917e7109f963d621513d533
Michael Hill <michael@bountyapp.co>
2017-07-25 15:46:54 -0700

    added c64_timed_writer

---
+++ c64_video_timed_writer/c64_video_timed_writer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  c64_video_timed_writer
+//
+//  Created by Michael Hill on 7/25/17.
+//  Copyright © 2017 Michael Hill. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
