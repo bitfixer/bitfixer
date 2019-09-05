0814c3c38aba5fec679ad59753d1509b1b8c3689
Michael Hill <gubbish@Michaels-MacBook-Pro.local>
2019-09-05 08:51:54 -0700

    petpix install update

--- petpix_install.sh
+++ petpix_install.sh
@@ -9,22 +9,6 @@ apt-get -y install ruby
 apt-get -y install php
 gem install rmate
 
-#install janus
-apt-get -y install libmicrohttpd-dev libjansson-dev \
-libssl-dev libsrtp-dev libsofia-sip-ua-dev libglib2.0-dev \
-libopus-dev libogg-dev libcurl4-openssl-dev liblua5.3-dev \
-libconfig-dev pkg-config gengetopt libtool automake libnice-dev
-apt-get -y remove libsrtp0-dev
-apt-get -y install libsrtp2-dev
-git clone https://github.com/meetecho/janus-gateway.git
-cd janus-gateway
-sh autogen.sh
-./configure --prefix=/opt/janus
-make
-make install
-make configs
-cd ..
-
 #install http-server
 apt-get -y install npm
 npm install http-server -g
@@ -36,10 +20,10 @@ rm ngrok-stable-linux-arm.zip
 cp /boot/config.txt config.txt; echo "core_freq=250" >> config.txt
 cp config.txt /boot/config.txt
 rm config.txt
-cp /boot/cmdline.txt cmdline.txt; echo "spidev.bufsiz=65535 " >> cmdline.txt; tr -d '\n' < cmdline.txt > cmdline_new.txt
-cp cmdline_new.txt /boot/cmdline.txt
-rm cmdline.txt; rm cmdline_new.txt
+#cp /boot/cmdline.txt cmdline.txt; echo "spidev.bufsiz=65535 " >> cmdline.txt; tr -d '\n' < cmdline.txt > cmdline_new.txt
+#cp cmdline_new.txt /boot/cmdline.txt
+#rm cmdline.txt; rm cmdline_new.txt
 
-git clone https://github.com/bitfixer/petpix_v2.git
-cd petpix_v2
-make all
+#git clone https://github.com/bitfixer/petpix_v2.git
+#cd petpix_v2
+#make all
